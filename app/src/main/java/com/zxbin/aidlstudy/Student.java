7507caf419eab004fe9c3880e4676192bab73a1a
rexcheung <zxbin_@163.com>
2017-12-26 23:23:49 +0800

    init

---
+++ app/src/main/java/com/zxbin/aidlstudy/Student.java
@@ -0,0 +1,53 @@
+package com.zxbin.aidlstudy;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Created by Administrator on 2017/12/26.
+ */
+
+public class Student implements Parcelable {
+
+
+
+    public static final int SEX_MALE = 1;
+    public static final int SEX_FEMALE = 2;
+
+    public int sno;
+    public String name;
+    public int sex;
+    public int age;
+
+    protected Student(Parcel in) {
+        sno = in.readInt();
+        name = in.readString();
+        sex = in.readInt();
+        age = in.readInt();
+    }
+
+    public static final Creator<Student> CREATOR = new Creator<Student>() {
+        @Override
+        public Student createFromParcel(Parcel in) {
+            return new Student(in);
+        }
+
+        @Override
+        public Student[] newArray(int size) {
+            return new Student[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(sno);
+        dest.writeString(name);
+        dest.writeInt(sex);
+        dest.writeInt(age);
+    }
+}
